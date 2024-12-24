a4ec42a01dea6e2944b52fcbb96cc094a2cc827d
George 🦜 <104024088+neuralcoral@users.noreply.github.com>
2024-12-24 10:24:54 -0500

    Added read from a file

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,17 @@
 // Created by George Alfaro on 12/17/24.
 //
 #include <iostream>
+#include <__filesystem/operations.h>
+
 #include "lexer.h"
 #include "parser.h"
+#include <fstream>
 
-int main() {
-  const auto source = "IF+-123 foo*THEN/";
+char* read_file(const char* file_name);
+
+int main(int argc, char* argv[]) {
+
+  const auto source = read_file(argv[1]);
   auto lexer = Lexer(source);
   auto parser = Parser(&lexer);
 
@@ -16,5 +22,30 @@ int main() {
     delete token;
     token = lexer.getToken();
   }
+  std::cout << "Parser completed." << std::endl;
+  delete[] source;
   return 0;
+}
+
+char* read_file(const char* file_name) {
+  if (file_name == nullptr) {
+    std::cerr << "File name cannot be empty." << std::endl;
+    throw std::invalid_argument("File name cannot be empty.");
+  }
+  char* fileContent;
+  if (std::filesystem::exists(file_name)) {
+    if (std::ifstream file(file_name); file.is_open()) {
+      const unsigned int fileSize = std::filesystem::file_size(file_name);
+      fileContent = new char[fileSize];
+      file.read(fileContent, fileSize);
+      file.close();
+    } else {
+      std::cerr << "File could not be opened." << std::endl;
+      throw std::runtime_error("File could not be opened.");
+    }
+  } else {
+    std::cerr << "File done not exist." << std::endl;
+    throw std::runtime_error("File does not exist.");
+  }
+  return fileContent;
 }
